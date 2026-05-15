93ea67ee513c3f04b836d617e6c7064653455fc8
Netherstar44 <josedavidcorreanunez123@gmail.com>
2026-05-14 23:09:57 -0500

    vercel 4444444444444

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
-import { registerRoutes } from "./routes";
+import { registerRoutes } from "./routes.ts";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 
